20fe6103cd8e4265ba595b77f248235b087cc95b
syogo-user <s.photo.trumpet.005@gmail.com>
2024-05-15 16:25:32 +0900

    データを一覧で表示する #3

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 小野寺祥吾 on 2024/05/15.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
