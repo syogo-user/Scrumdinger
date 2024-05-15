8a7e9f9f00500f82b7030c0d461c2c1ddb35955c
syogo-user <s.photo.trumpet.005@gmail.com>
2024-05-15 11:16:18 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 小野寺祥吾 on 2024/05/15.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
